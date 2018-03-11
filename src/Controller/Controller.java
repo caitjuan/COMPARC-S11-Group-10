4bb1db2de0889303fbe741061d1dab5a2ae88481
Caitlienne Diane Juan <caitliennedianejuan@gmail.com>
2018-03-11 22:36:02 +0800

    update error checking for LD and SD

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -26,57 +26,60 @@ public class Controller {
         }while(error.contentEquals(" "));
     }
     
-//    private String errorCheck(ArrayList<String> code){
-//        boolean isError = false;
-//        int size = code.size();
-//        
-//        for(int i = 0; i < size; i++){
-//            if (code.get(i).startsWith("LD") || code.get(i).startsWith("SD")){ /*LD rt, offset(base)*/ /*SD rt, offset(base)*/
-//            
-//            String jString;
-//            String rt = /*DONT KNOW. ASK CAIT/JAI HOW TO GET JUST THE RT PART. R0-R31*/
-//            String base  = ^
-//
-//            int j = 0;
-//            int exit1 = exit2 = 0;
-//
-//            /*rt part*/
-//            while(j < 32 && exit1 == 0)
-//            {
-//                    jString = Integer.toString(j);		/*Only number. No "R" of R1*/
-//                    checking = rt.endsWith(jString);
-//
-//                    if (checking)
-//                            exit1 = 1;
-//            }
-//
-//            if (checking)
-//            {
-//                    j = 0;
-//                    /*base part*/
-//                    while(j < 32 && exit2 == 0)
-//                    {
-//                            jString = Integer.toString(j);		/*Only number. No "R" of R1*/
-//                            checking = base.endsWith(jString);
-//
-//                            if (checking)
-//                                    exit2 = 1;
-//
-//                    } /*No Code for checking offset yet. Offset is 1000-1FFF? so I have to make sure its only between 1000-1FFF?*/
-//            }
-//            else
-//            {
-//                    System.out.print("Syntax Error");
-//            }
-//        }
+    private String errorCheck(ArrayList<String> code){
+        boolean isError = false;
+        int size = code.size();
+        
+        for(int i = 0; i < size; i++){
+            if (code.get(i).startsWith("LD") || code.get(i).startsWith("SD")){ /*LD rt, offset(base)*/ /*SD rt, offset(base)*/
+            
+            String jString;
+            String rt = code.get(i).substring(2, 5);
+            String base  = code.get(i).substring(12);
+
+            int j = 0;
+            int exit1, exit2;
+            boolean checking = true;
+            
+            exit1 = exit2 = 0;
+
+            /*rt part*/
+            while(j < 32 && exit1 == 0)
+            {
+                    jString = Integer.toString(j);		/*Only number. No "R" of R1*/
+                    checking = rt.contains(jString);
+
+                    if (checking)
+                        exit1 = 1;
+            }
+
+            if (checking)
+            {
+                j = 0;
+                /*base part*/
+                while(j < 32 && exit2 == 0)
+                {
+                        jString = Integer.toString(j);		/*Only number. No "R" of R1*/
+                        checking = base.contains(jString);
+
+                        if(checking)
+                            exit2 = 1;
+
+                } /*No Code for checking offset yet. Offset is 1000-1FFF? so I have to make sure its only between 1000-1FFF?*/
+            }
+            else
+            {
+                    System.out.print("Syntax Error");
+            }
+        }
 //        else if (line.startsWith("DADDIU"))		/*DADDIU rt, rs, immediate. DADDIU R1, R0, #0002*/
 //        else if (line.startsWith("XORI"))		/*XORI rt, rs, immediate. XORI R10, R2, #FFFF*/
 //        else if (line.startsWith("BLTZ"))		/*BLTZ rs, offset. BLTZ R1, L1 */
 //        else if (line.startsWith("DADDU"))		/*DADDU rd, rs, rt. DADDU R4, R1, R2*/
 //        else if (line.startsWith("SLT"))		/*SLT rd, rs, rt. SLT R3, R1, R2*/
 //        else if (line.startsWith("BC"))			/*BC offset. BC L2*/
-//
-//        /*How to check LD/SD offset, immediate, branch offset*/
-//            
+
+        /*How to check LD/SD offset, immediate, branch offset*/
+            
     }
 }
